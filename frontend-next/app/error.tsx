05456120e7e09924a83658e14a10c904104b2de2
Mariya Miroshnikova <mari.mirosh@bk.ru>
2026-04-17 05:43:21 +0300

    make loading and error root pages

--- frontend-next/app/error.tsx
+++ frontend-next/app/error.tsx
@@ -1,7 +1,13 @@
 'use client';
 
-export default function ErrorWrapper({error}: {error: Error}) {
-    return(
-        <h1>Ooops! {error.message}</h1>
+import { Alert, AlertTitle, Box } from "@mui/material";
+
+export default function ErrorWrapper({ error }: { error: Error }) {
+    return (
+        <Box sx={{ display: "flex", justifyContent: "center", background: "#f5f5f5",  alignItems: "center", height: "100vh" }}>
+            <Alert severity="error" sx={{ display: "flex" }}>
+                <AlertTitle>Error</AlertTitle>
+                {error.message}</Alert>
+        </Box>
     )
 }
--- frontend-next/app/loading.tsx
+++ frontend-next/app/loading.tsx
@@ -1,5 +1,9 @@
+import { Box, CircularProgress, Typography } from "@mui/material";
+
 export default function LoadingAds() {
     return(
-        <h1>Loading...</h1>
+        <Box sx={{display:"flex", justifyContent: "center", background: "#f5f5f5", height: "100vh" }}>
+            <CircularProgress aria-label="Loading…" sx={{mt: 4}}/>
+        </Box>
     )
 }
