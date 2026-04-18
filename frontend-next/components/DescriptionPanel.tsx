7574dc77bd28a1d9372f24991e317486f34d630d
Mariya Miroshnikova <mari.mirosh@bk.ru>
2026-04-18 11:02:31 +0300

    add styles to /id page (description panel)

--- frontend-next/components/DescriptionPanel.tsx
+++ frontend-next/components/DescriptionPanel.tsx
@@ -12,26 +12,34 @@ const DescriptionPanel = ({ data }: DescriptionPanelProps) => {
 
 
     return (
-        <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 0.5 }}>
             <Box sx={{ display: "flex", gap: 2 }}>
-                <Typography variant="h6"><b>Продавец:</b> {data.seller.name}</Typography>
+                <Typography variant="h6">{data.seller.name}</Typography>
                 <Rating value={Number(data.seller.rating)} sx={{ alignSelf: "center" }} readOnly />
             </Box>
-            <Typography> Объявлений: {data.seller.totalAds} | На сайте: {getFormattedDurationString(data.seller.registeredAt)}</Typography>
-            <Typography variant="h6">Характеристики</Typography>
-            <table>
+            <Typography color="textSecondary"> Объявлений: {data.seller.totalAds} | На сайте: {getFormattedDurationString(data.seller.registeredAt)}</Typography>
+            <Typography variant="h6" sx={{mt: 2}}>Характеристики</Typography>
+            <table style={{ width: "fit-content" }}>
                 <tbody>
                     {Object.entries(data.characteristics).map((i, index) => {
                         return (
                             <tr key={index}>
-                                <td>{i[0]}</td>
-                                <td>{i[1]}</td>
+                                <td style={{ paddingRight: 30 }}>
+                                    <Typography color="textSecondary">
+                                        {i[0]}
+                                    </Typography>
+                                </td>
+                                <td>
+                                    <Typography>
+                                        {i[1]}
+                                    </Typography>
+                                </td>
                             </tr>
                         )
                     })}
                 </tbody>
             </table>
-            <Typography variant="h6" color="in">Полное описание</Typography>
+            <Typography variant="h6" color="in" sx={{mt: 2}}>Полное описание</Typography>
             <Typography variant="body1">{data.description}</Typography>
         </Box>
     )
